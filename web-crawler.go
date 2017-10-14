6106d2e522ef1fbdf929ba70c5a70f7478133579
theLastTrain <wangzhihao9110@gmail.com>
2017-10-14 10:17:58 -0500

    Create web-crawler.go

---
+++ web-crawler.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Fetcher interface {
+	// Fetch 返回 URL 的 body 内容，并且将在这个页面上找到的 URL 放到一个 slice 中。
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+type SafeCached struct {
+	cached map[string][]string
+	mt     sync.Mutex
+}
+
+var sc = SafeCached{cached: make(map[string][]string)}
+
+// Crawl 使用 fetcher 从某个 URL 开始递归的爬取页面，直到达到最大深度。
+func Crawl(url string, depth int, fetcher Fetcher) {
+	// TODO: 不重复抓取页面。
+	// 下面并没有实现上面两种情况：
+	if depth <= 0 {
+		return
+	}
+
+	sc.mt.Lock()
+	if _, ok := sc.cached[url]; ok {
+		fmt.Printf("==> %s already fetched\n", url)
+		sc.mt.Unlock()
+		return
+	}
+	sc.mt.Unlock()
+
+	body, urls, err := fetcher.Fetch(url)
+
+	sc.mt.Lock()
+	sc.cached[url] = append(sc.cached[url], urls...)
+	sc.mt.Unlock()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("found: %s %q\n", url, body)
+	done := make(chan bool)
+	for _, u := range urls {
+		go func(url string) {
+			Crawl(url, depth-1, fetcher)
+			done <- true
+		}(u)
+	}
+	for i, u := range urls {
+		fmt.Printf("<- [%v] %v/%v Waiting for child %v.\n", url, i, len(urls), u)
+		<-done
+	}
+	return
+}
+
+func main() {
+	Crawl("http://golang.org/", 4, fetcher)
+}
+
+// fakeFetcher 是返回若干结果的 Fetcher。
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher 是填充后的 fakeFetcher。
+var fetcher = fakeFetcher{
+	"http://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"http://golang.org/pkg/",
+			"http://golang.org/cmd/",
+		},
+	},
+	"http://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/cmd/",
+			"http://golang.org/pkg/fmt/",
+			"http://golang.org/pkg/os/",
+		},
+	},
+	"http://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/pkg/",
+		},
+	},
+	"http://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/pkg/",
+		},
+	},
+}
